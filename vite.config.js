413fd1c48346de2efcf931de0983ca45fe7de540
Arturo Quiros <adquirose@gmail.com>
2025-01-08 13:34:42 -0300

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   plugins: [
     react(),
     svgr({
